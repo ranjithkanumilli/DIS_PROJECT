c48257deb1eaeb9d866d5bdf840734872ad031fc
Ranjith kanumilli <kanumilli1@usf.edu>
2023-04-16 15:30:15 -0400

    added youtube links

--- script/script-product.js
+++ script/script-product.js
@@ -5,6 +5,7 @@ const API_KEY = 'api_key=f55104d0cb044a5ac6b66815a2732922';
 const IMG_URL = 'https://image.tmdb.org/t/p/original';
 
 const productdetails = document.getElementsByClassName("product-details-main")[0];
+const actionButton = document.getElementsByClassName("action-buttons")[0];
 
 // Get the movie title from the URL query parameter
 const urlParams = new URLSearchParams(window.location.search);
@@ -12,31 +13,43 @@ const movieid = urlParams.get("id");
 const type = urlParams.get("type");
 console.log(type)
 productdetails.innerHTML = "";
-
+actionButton.innerHTML = "";
 
 // Fetch the movie details from your API or database
 fetch(
-    `${BASE_URL}/${type}/${movieid}?${API_KEY}&language=en-US`
+    `${BASE_URL}/${type}/${movieid}?${API_KEY}&language=en-US&append_to_response=videos`
 )
     .then((response) => response.json())
     .then((data) => {
+        const youtubeKey = data.videos.results[0].key;
+        // Construct YouTube link
+        const youtubeLink = `https://www.youtube.com/watch?v=${youtubeKey}`;
+
         // Display the movie details on the page
-        const {title ,name, overview, vote_average, backdrop_path } = data;
+        const { title, name, overview, vote_average, backdrop_path } = data;
 
         const moviedetail = document.createElement("div");
         moviedetail.classList.add("product-main");
         moviedetail.innerHTML = `
-    <div class="product-image">
-            <img src="${IMG_URL + backdrop_path}" alt="">
-        </div>
+        <div class="product-image">
+        <img src="${IMG_URL + backdrop_path}" alt="">
+    </div>
     <div class="product-details">
-    <h2>${title ? title : name} - IMDB :  ${vote_average}</h2>
-    <p>${overview}</p>
-       
-            </div>
-           
+        <h2>${title ? title : name} - IMDB : ${vote_average}</h2>
+        <p>${overview}</p>
+    
+    </div>
             `;
         productdetails.appendChild(moviedetail);
+
+
+        const actionlink = document.createElement("div");
+        actionlink.classList.add("link");
+        actionlink.innerHTML = ` 
+        <button><a target="_blank" href = ${youtubeLink}>Watch Now</a></button>
+        `
+        actionButton.appendChild(actionlink)
+
     })
     .catch((error) => {
         console.error("Error fetching movie details:", error);

8eb2f18d539c1f49e42212cba4b4f6b0c839772c
Ranjith kanumilli <kanumilli1@usf.edu>
2023-04-16 11:26:00 -0400

    updated carousel in homepage

--- script/script.js
+++ script/script.js
@@ -5,12 +5,13 @@ const API_KEY = 'api_key=f55104d0cb044a5ac6b66815a2732922';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const IMG_URL_ORIGINAL = 'https://image.tmdb.org/t/p/original';
 
 const TRENDING_MOVIES_URL = BASE_URL + '/trending/movie/week?' + API_KEY;
 const TRENDING_TV_URL = BASE_URL + '/trending/tv/week?' + API_KEY;
 
 const trendingmovies = document.getElementsByClassName('carousel-container')[0];
-const trendingtv = document.getElementsByClassName('carousel-container')[1];
+const trendingtv = document.getElementsByClassName('carousel-container-tv')[0];
 
 
 
@@ -20,82 +21,65 @@ getTrendingTv(TRENDING_TV_URL);
 
 function getTrendingMovies(url) {
 
-    fetch(url).then(res => res.json()).then(data => {
-        showTrendingMovies(data.results);
-    })
+  fetch(url).then(res => res.json()).then(data => {
+    showTrendingMovies(data.results);
+    generateCarouselItems(data.results);
+  })
 
 }
 
 function getTrendingTv(url) {
 
-    fetch(url).then(res => res.json()).then(data => {
-        showTrendingTv(data.results);
-        console.log(data.results)
-    })
+  fetch(url).then(res => res.json()).then(data => {
+    showTrendingTv(data.results);
+  })
 
 }
 
 
 
+//Carousel for trending movies
+const carouselContainer = document.querySelector('.carousel-container');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let scrollAmount = 0;
+const itemsInView = 5;
+const viewWidth = carouselContainer.offsetWidth / itemsInView;
+let data = [];
 
-const carouselContainers = document.querySelectorAll('.carousel-container');
-// const prevButton = document.querySelector('.prev-button');
-// const nextButton = document.querySelector('.next-button');
-// let scrollAmount = 0;
-// const itemsInView = 5;
-// const viewWidth = carouselContainer.offsetWidth / itemsInView;
-// let data = [];
-
-
-
-
-carouselContainers.forEach(carouselContainer => {
-    const prevButton = document.getElementsByClassName('prev-button')[0];
-    const nextButton = document.getElementsByClassName('next-button')[0];
-    let scrollAmount = 0;
-    const itemsInView = 5;
-    const viewWidth = carouselContainer.offsetWidth / itemsInView;
-    let data = [];
-
-    // Add event listeners to previous and next buttons
-
-    nextButton.addEventListener('click', () => {
-        scrollAmount += viewWidth;
-        if (scrollAmount > carouselContainer.scrollWidth - viewWidth) {
-            scrollAmount = carouselContainer.scrollWidth - viewWidth;
-        }
-        carouselContainer.scrollTo({
-            top: 0,
-            left: scrollAmount,
-            behavior: 'smooth'
-        });
-    });
+nextButton.addEventListener('click', () => {
+  scrollAmount += viewWidth;
+  if (scrollAmount > carouselContainer.scrollWidth - viewWidth) {
+    scrollAmount = carouselContainer.scrollWidth - viewWidth;
+  }
+  carouselContainer.scrollTo({
+    top: 0,
+    left: scrollAmount,
+    behavior: 'smooth'
+  });
+});
 
 
-    prevButton.addEventListener('click', () => {
-        scrollAmount -= viewWidth;
-        if (scrollAmount < 0) {
-            scrollAmount = 0;
-        }
-        carouselContainer.scrollTo({
-            top: 0,
-            left: scrollAmount,
-            behavior: 'smooth'
-        });
-    });
-
-
-
+prevButton.addEventListener('click', () => {
+  scrollAmount -= viewWidth;
+  if (scrollAmount < 0) {
+    scrollAmount = 0;
+  }
+  carouselContainer.scrollTo({
+    top: 0,
+    left: scrollAmount,
+    behavior: 'smooth'
+  });
 });
 
 function showTrendingMovies(data) {
-    trendingmovies.innerHTML = '';
+  trendingmovies.innerHTML = '';
 
-    data.forEach(item => {
-        const { title, poster_path, vote_average, id } = item;
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('carousel-item');
-        movieEl.innerHTML = `
+  data.forEach(item => {
+    const { title, poster_path, vote_average, id } = item;
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('carousel-item');
+    movieEl.innerHTML = `
         <a href="#" onclick="showMovieDetails(${id})">
             <img src="${IMG_URL + poster_path}" alt="${title}">
         </a>
@@ -104,18 +88,57 @@ function showTrendingMovies(data) {
 
         `
 
-        trendingmovies.appendChild(movieEl);
-    })
+    trendingmovies.appendChild(movieEl);
+  })
 }
 
-function showTrendingTv(data) {
-    trendingtv.innerHTML = '';
 
-    data.forEach(item => {
-        const { name, poster_path, vote_average, id } = item;
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('carousel-item');
-        movieEl.innerHTML = `
+
+
+
+//Carousel for trending TV
+const carouselContainerTv = document.querySelector('.carousel-container-tv');
+const prevButtonTv = document.querySelector('.prev-button-tv');
+const nextButtonTv = document.querySelector('.next-button-tv');
+let scrollAmountTv = 0;
+const itemsInViewTv = 5;
+const viewWidthTv = carouselContainerTv.offsetWidth / itemsInView;
+let dataTv = [];
+
+nextButtonTv.addEventListener('click', () => {
+  scrollAmountTv += viewWidthTv;
+  if (scrollAmountTv > carouselContainerTv.scrollWidthTv - viewWidthTv) {
+    scrollAmountTv = carouselContainerTv.scrollWidthTv - viewWidthTv;
+  }
+  carouselContainerTv.scrollTo({
+    top: 0,
+    left: scrollAmountTv,
+    behavior: 'smooth'
+  });
+});
+
+
+prevButtonTv.addEventListener('click', () => {
+  scrollAmountTv -= viewWidthTv;
+  if (scrollAmountTv < 0) {
+    scrollAmountTv = 0;
+  }
+  carouselContainerTv.scrollTo({
+    top: 0,
+    left: scrollAmountTv,
+    behavior: 'smooth'
+  });
+});
+
+
+function showTrendingTv(data) {
+  trendingtv.innerHTML = '';
+
+  data.forEach(item => {
+    const { name, poster_path, vote_average, id } = item;
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('carousel-item-tv');
+    movieEl.innerHTML = `
         <a href="#" onclick="showMovieDetails(${id})">
             <img src="${IMG_URL + poster_path}" alt="${name}">
         </a>
@@ -123,47 +146,18 @@ function showTrendingTv(data) {
         <p>imdb:${vote_average}</p>
 
         `
-        trendingtv.appendChild(movieEl);
-    })
+    trendingtv.appendChild(movieEl);
+  })
 }
 
 
 
 
-// // Add event listeners to previous and next buttons
-// nextButton.addEventListener('click', () => {
-//     scrollAmount += viewWidth;
-//     if (scrollAmount > carouselContainer.scrollWidth - viewWidth) {
-//         scrollAmount = carouselContainer.scrollWidth - viewWidth;
-//     }
-//     carouselContainer.scrollTo({
-//         top: 0,
-//         left: scrollAmount,
-//         behavior: 'smooth'
-//     });
-// });
-
-// prevButton.addEventListener('click', () => {
-//     scrollAmount -= viewWidth;
-//     if (scrollAmount < 0) {
-//         scrollAmount = 0;
-//     }
-//     carouselContainer.scrollTo({
-//         top: 0,
-//         left: scrollAmount,
-//         behavior: 'smooth'
-//     });
-// });
-
-
 
 
 function showMovieDetails(id) {
-    // This function will open a new page to show the details of the selected movie
-    // You can use the title parameter to fetch the movie details from your API or database
 
-    // Example code to redirect to the details page:
-    window.location.href = `./Pages/Product.html?id=${id}`;
+  window.location.href = `./Pages/Product.html?id=${id}`;
 
 }
 
@@ -173,10 +167,95 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const searchTerm = search.value;
+  e.preventDefault();
+  const searchTerm = search.value;
 
-    if(searchTerm){
-        getTrendingMovies(searchURL + '&query=' + searchTerm)
-    }
+  if (searchTerm) {
+    getTrendingMovies(searchURL + '&query=' + searchTerm)
+  }
 })
+
+
+
+// carousel hero - home page
+
+const carousel = document.querySelector(".carousel-hero");
+const carouselContainerhero = document.querySelector(".carousel-container-hero");
+const prevBtn = document.querySelector(".carousel-hero-prev");
+const nextBtn = document.querySelector(".carousel-hero-next");
+
+let currentIndex = 0;
+let intervalId;
+
+// Function to dynamically generate carousel items
+function generateCarouselItems(data) {
+  let carouselItemsHTML = "";
+  data.forEach((item) => {
+    const { backdrop_path, title } = item;
+    carouselItemsHTML += `
+      <div class="carousel-hero-item">
+        <img src="${IMG_URL_ORIGINAL + backdrop_path}">
+        <div class="carousel-hero-title">${title}</div>
+      </div>
+    `;
+  });
+  carouselContainerhero.innerHTML = carouselItemsHTML;
+}
+
+
+
+// Function to go to a specific slide
+function goToSlide(index) {
+  carouselContainerhero.style.transform = `translateX(-${index * carousel.offsetWidth}px)`;
+  currentIndex = index;
+}
+
+// Function to go to the next slide
+function goToNextSlide() {
+  if (currentIndex === carouselContainerhero.children.length - 1) {
+    goToSlide(0);
+  } else {
+    goToSlide(currentIndex + 1);
+  }
+}
+
+// Function to go to the previous slide
+function goToPrevSlide() {
+  if (currentIndex === 0) {
+    goToSlide(carouselContainerhero.children.length - 1);
+  } else {
+    goToSlide(currentIndex - 1);
+  }
+}
+
+// Resize carousel items on window resize
+window.addEventListener("resize", () => {
+  resizeCarouselItems();
+  goToSlide(currentIndex);
+});
+
+// Handle previous and next button clicks
+prevBtn.addEventListener("click", () => {
+  goToPrevSlide();
+  clearInterval(intervalId);
+});
+
+nextBtn.addEventListener("click", () => {
+  goToNextSlide();
+  clearInterval(intervalId);
+});
+
+// Automatically transition to the next slide every 5 seconds
+intervalId = setInterval(() => {
+  goToNextSlide();
+}, 5000);
+
+
+
+
+
+
+
+
+
+
